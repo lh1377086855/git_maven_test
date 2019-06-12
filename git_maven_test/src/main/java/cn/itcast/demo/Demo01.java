40a587dad981e2d6177a96bce68ac821a2503a62
luohao <1377086855@qq.com>
2019-06-12 20:59:43 +0800

    添加maven工程

---
+++ git_maven_test/src/main/java/cn/itcast/demo/Demo01.java
@@ -0,0 +1,15 @@
+package cn.itcast.demo;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("1233");
+        System.out.println("1233");
+        System.out.println("1233");
+        System.out.println("1233");
+        System.out.println("1233");
+    }
+    public void test(){
+        int i = 0;
+        System.out.println(i);
+    }
+}
